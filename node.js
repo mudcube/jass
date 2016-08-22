39bd4ae89adb8fd4915abdb36926b8786bc7a44d
Michael Deal <mudcube@gmail.com>
2016-08-22 11:30:54 -0700

    twiddle with test

--- node.js
+++ node.js
@@ -1,4 +1,4 @@
 var fs = require('fs')
-var jassParse = require('./parser')
-var sass = jassParse(fs.readFileSync('./test.jcss', 'utf8'))
+var jcssParse = require('./parser')
+var sass = jcssParse(fs.readFileSync('./test.jcss', 'utf8'))
 console.log(sass)
